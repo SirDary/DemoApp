c119bb748b6dd8ddda86e2cc65b248660adf9f28
Сардарбек Акматалиев <akmatalievsardar@gmail.com>
2025-06-05 17:54:03 +0300

    Починил tab-pills

--- Demo_Auth/Pages/TaskManager/Index.cshtml.cs
+++ Demo_Auth/Pages/TaskManager/Index.cshtml.cs
@@ -1,20 +1,36 @@
 using Demo_Auth.Data;
 using Demo_Auth.Data.Models;
 using Demo_Auth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Pages.TaskManager
 {
-    public class IndexModel(TaskService taskService) : PageModel
+    [Authorize]
+    public class IndexModel(TaskService taskService, UserManager<AppUser> userManager, TaskManagerDbContext dbContext) : PageModel
     {
-        public IEnumerable<Tasks> Tasks { get; set; } = [];
+        public IEnumerable<Tasks> AllTasks { get; set; } = [];
+        public IEnumerable<Tasks> MeTasks { get; set; } = [];
+        public IEnumerable<Tasks> MyTasks { get; set; } = [];
         public IEnumerable<Tasks> OverdueTasks { get; set; } = [];
         public async Task<IActionResult> OnGet()
         {
-            Tasks = await taskService.GetAllTasksWithDetailsAsync();
-            OverdueTasks = Tasks.Where(t => t.Deadline < DateTime.Now).ToList();
-
+            var user = dbContext.Users
+                .Include(u => u.Employee)
+                .Where(u => u.Id == userManager.GetUserId(User))
+                .FirstOrDefault();
+            
+            if(user is not null)
+            {
+                AllTasks = await taskService.GetAllTasksWithDetailsAsync();
+                OverdueTasks = AllTasks.Where(t => t.Deadline < DateTime.Now).ToList();
+                MyTasks = AllTasks.Where(t => t.TaskAuthorNavigation.EmployeeId == user.Employee.EmployeeId).ToList();
+                MeTasks = AllTasks.Where(t => t.Executors.Contains(user.Employee)).ToList();
+            }
+            
             return Page();
         }
 
@@ -24,5 +40,6 @@ namespace Demo.Pages.TaskManager
                 return RedirectToPage("/TaskManager/Create");
             return Page();
         }
+
     }
 }
